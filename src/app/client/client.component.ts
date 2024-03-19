12c69f59e75da342343cfc92ed7109e0fc8e31f8
arturcardoso <artur.cardoso097@gmail.com>
2024-03-19 09:37:32 -0300

    cadastro de aluno

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Student } from './student';
+
 
 @Component({
   selector: 'app-client',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './client.component.css'
 })
 export class ClientComponent {
-
+  students: Student[] = [
+    {id: 1, name: "Artur", course: "Computação"},
+    {id: 2, name: "Renato", course: "Computação"},
+    {id: 3, name: "Amarga", course: "Matemática"},
+  ];
+  
 }
